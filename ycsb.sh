265d472658a53450600cf7d396127edbefd64e65
Liang Geng <geng.161@osu.edu>
2022-06-11 20:49:29 -0400

    Cleanup scripts

--- ycsb.sh
+++ ycsb.sh
@@ -1,36 +1,86 @@
 set -e
-export RDMA_VERBOSITY=ERROR
+export KVSTORE_HOME=/home/geng.161/Projects/gRPC-KVStore/build
 
-rdma_mode=""
-if [[ $GRPC_PLATFORM_TYPE == "RDMA_EVENT" ]]; then
-  rdma_mode="event"
-elif [[ $GRPC_PLATFORM_TYPE == "RDMA_BP" ]]; then
-  rdma_mode="bp"
-elif [[ $GRPC_PLATFORM_TYPE == "TCP" ]]; then
-  rdma_mode="tcp"
-else
-  echo "Need valid ENV GRPC_PLATFORM_TYPE"
-  exit 1
-fi
-
-export LOG_SUFFIX=$rdma_mode
-
-export KVSTORE_HOME=
-
-hostfile_template="$1"
+hostfile_template="ycsb/hosts"
 hostfile_template=$(realpath "$hostfile_template")
 if [[ ! -f "$hostfile_template" ]]; then
   echo "Bad hostfile $hostfile_template"
   exit 1
 fi
 
-for n_clients in 1 2 4 8 16 32 64; do
+function set_hostfile() {
+  n_clients=$1
   name_prefix=$(basename "$hostfile_template")
-  hostfile="/tmp/$name_prefix.${RANDOM}"
-  while [[ -f "$hostfile" ]]; do
-    hostfile="/tmp/$name_prefix.${RANDOM}"
-  done
-  ./gen_hostfile.py ./ycsb/hosts $n_clients > "$hostfile"
+  hostfile="/tmp/$name_prefix"
+  ./gen_hostfile.py ycsb/hosts "$n_clients" >"$hostfile"
   export HOSTS_PATH="$hostfile"
-  ./ycsb/ycsb.sh -c=run -p="$(realpath ycsb/workload.dat)" --async --thread=28
+}
+
+GRPC_MODES=(TCP RDMA_BP RDMA_BPEV RDMA_EVENT)
+
+function clean() {
+  profile="$1"
+  ./ycsb/ycsb.sh -c=clean -p="$(realpath ycsb/"$profile")"
+}
+
+function load() {
+  profile="$1"
+  export GRPC_PLATFORM_TYPE=TCP
+  set_hostfile 1
+  ./ycsb/ycsb.sh -c=load -p="$(realpath ycsb/"$profile")" --async --thread=1
+}
+
+function scalability() {
+  profile="$1"
+  N_CLIENTS=(1 2 4 8 16 32 64 128)
+
+  for grpc_mode in "${GRPC_MODES[@]}"; do
+    export GRPC_PLATFORM_TYPE=$grpc_mode
+    for n_cli in "${N_CLIENTS[@]}"; do
+      set_hostfile "$n_cli"
+
+      thread=28
+      bp_to=0
+      if [[ n_cli -le 32 ]]; then
+        if [[ "${grpc_mode}" == "RDMA_BPEV" ]]; then
+          thread=27
+        else
+          thread=28
+        fi
+        bp_to=50
+      elif [[ n_cli -le 64 ]]; then
+        thread=32
+      else
+        thread=64 # for 128 client
+      fi
+
+      export LOG_SUFFIX="th_${thread}"
+      ./ycsb/ycsb.sh -c=run -p="$(realpath ycsb/"$profile")" --async --thread="$thread" --bp-timeout=$bp_to
+    done
+  done
+}
+
+for i in "$@"; do
+  case $i in
+  --clean=*)
+    clean "${i#*=}"
+    shift
+    ;;
+  --load=*)
+    load "${i#*=}"
+    shift
+    ;;
+  --scalability=*)
+    scalability "${i#*=}"
+    shift
+    ;;
+  --* | -*)
+    echo "Unknown option $i"
+    exit 1
+    ;;
+  *)
+    echo "--bench or --profile"
+    exit 1
+    ;;
+  esac
 done
--- ycsb/ycsb.sh
+++ ycsb/ycsb.sh
@@ -2,19 +2,19 @@
 #set -e
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 if [[ -z "$HOSTS_PATH" ]]; then
-  echo "Using default $HOSTS_PATH"
   HOSTS_PATH="$SCRIPT_DIR/hosts"
+  echo "Using default $HOSTS_PATH"
 fi
 DB_PREFIX="/tmp/rocks.db"
 SERVER=$(head -n 1 "$HOSTS_PATH")
 FIRST_CLIENT=$(head -n 2 "$HOSTS_PATH" | tail -n 1 | cut -d" " -f1,1)
-NP=$(awk '{ sum += $1 } END { print sum }' <(tail -n +2 "$HOSTS_PATH" |cut -d"=" -f2,2))
-MPI_LIB=$(realpath "$(which mpirun|xargs dirname)"/../lib)
+NP=$(awk '{ sum += $1 } END { print sum }' <(tail -n +2 "$HOSTS_PATH" | cut -d"=" -f2,2))
+MPI_LIB=$(realpath "$(which mpirun | xargs dirname)"/../lib)
 ASYNC=false
 THREAD=$(nproc)
 
 if [[ -z "$WORKLOADS" ]]; then
-  WORKLOADS="workloada workloadb workloadc workloadd workloade workloadf tsworkloada"
+  WORKLOADS="workloada workloadb workloadc workloadd workloade workloadf"
 fi
 
 if [[ ! -f "$YCSB_HOME/bin/ycsb" ]]; then
@@ -29,28 +29,31 @@ fi
 
 for i in "$@"; do
   case $i in
-    -c=*|--cmd=*)
-      CMD="${i#*=}"
-      shift
-      ;;
-    -p=*|--profile=*)
-      PROFILE_PATH="${i#*=}"
-      shift
-      ;;
-    --async)
-      ASYNC=true
-      shift
-      ;;
-    -t=*|--thread=*)
-      THREAD="${i#*=}"
-      shift
-      ;;
-    -*|--*)
-      echo "Unknown option $i"
-      exit 1
-      ;;
-    *)
-      ;;
+  -c=* | --cmd=*)
+    CMD="${i#*=}"
+    shift
+    ;;
+  -p=* | --profile=*)
+    PROFILE_PATH="${i#*=}"
+    shift
+    ;;
+  --async)
+    ASYNC=true
+    shift
+    ;;
+  -t=* | --thread=*)
+    THREAD="${i#*=}"
+    shift
+    ;;
+  --bp-timeout=*)
+    export GRPC_BP_TIMEOUT="${i#*=}"
+    shift
+    ;;
+  --* | -*)
+    echo "Unknown option $i"
+    exit 1
+    ;;
+  *) ;;
   esac
 done
 
@@ -61,103 +64,116 @@ else
   exit 1
 fi
 
-LOG_PATH=$(realpath "$SCRIPT_DIR/logs/logs_$NP")
-if [[ $ASYNC == true ]]; then
-  LOG_PATH="${LOG_PATH}_async"
-else
-  LOG_PATH="${LOG_PATH}_sync"
-fi
-if grep -q "worker.proportion=" < "$PROFILE_PATH"; then
-  proportion=$(grep "worker.proportion=" < "$PROFILE_PATH" | cut -d"=" -f2,2)
-  LOG_PATH="${LOG_PATH}_${proportion}"
-fi
+function get_logs_dir() {
+    LOG_PATH=$(realpath "$SCRIPT_DIR/logs")
+    LOG_PATH="$LOG_PATH/${GRPC_PLATFORM_TYPE}_$NP"
+    if [[ $ASYNC == true ]]; then
+      LOG_PATH="${LOG_PATH}_async"
+    else
+      LOG_PATH="${LOG_PATH}_sync"
+    fi
 
-if [[ -n "$LOG_SUFFIX" ]]; then
-  LOG_PATH="${LOG_PATH}_${LOG_SUFFIX}"
-fi
-mkdir -p "$LOG_PATH"
-export RDMA_VERBOSITY=ERROR
-
-function start_server() {
-  workload=$1
-  db_file="${DB_PREFIX}/${workload}"
-  # Launch server
-  mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x RDMA_VERBOSITY \
-         -n 1 -host "$SERVER" \
-         "$KVSTORE_HOME"/kv_store -server -async=$ASYNC -db_file="$db_file" -thread="$THREAD" &
+    if [[ -n "$LOG_SUFFIX" ]]; then
+      LOG_PATH="${LOG_PATH}_${LOG_SUFFIX}"
+    fi
+    mkdir -p "$LOG_PATH"
+    echo "$LOG_PATH"
 }
+
 
 function kill_server() {
   ssh "$SERVER" 'if ps aux | pgrep kv_store >/dev/null; then pkill kv_store && while $(pkill -0 kv_store 2>/dev/null); do continue; done; fi'
-#  ssh "$SERVER" 'ps aux|pgrep kv_store|xargs kill -9 || true'
+  #  ssh "$SERVER" 'ps aux|pgrep kv_store|xargs kill -9 || true'
+}
+
+function get_db_file() {
+  workload="$1"
+  profile_name=$(basename "$PROFILE_PATH")
+  db_file="${DB_PREFIX}/${workload}_$profile_name"
+  echo "$db_file"
+}
+
+function start_server() {
+  kill_server
+  workload="$1"
+  db_file="$(get_db_file "$workload")"
+  # Launch server
+  mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x GRPC_BP_TIMEOUT \
+    -n 1 -host "$SERVER" \
+    "$KVSTORE_HOME"/kv_store -server -async=$ASYNC -db_file="$db_file" -thread="$THREAD" &
 }
 
 function load() {
-    workload=$1
-    db_file="${DB_PREFIX}/${workload}"
+  workload=$1
+  db_file="$(get_db_file "$workload")"
+  if ssh "$SERVER" "[ -d $db_file ]"; then
+    echo "DB $workload exists, skip"
+  else
     if ssh "$SERVER" mkdir -p "$db_file"; then
       start_server "$workload"
       echo "Loading $workload"
-      mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x RDMA_VERBOSITY -n 1 -wdir "$YCSB_HOME" -host "$FIRST_CLIENT" \
-          python2 "$YCSB_HOME"/bin/ycsb load grpcrocksdb \
-            -jvm-args="-Djava.library.path=${KVSTORE_HOME}:${MPI_LIB}" \
-            -P "$YCSB_HOME/workloads/$workload" \
-            -P "$PROFILE_PATH" \
-            -s -p grpc.addr="$SERVER:12345"
+      mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x GRPC_BP_TIMEOUT -n 1 -wdir "$YCSB_HOME" -host "$FIRST_CLIENT" \
+        python2 "$YCSB_HOME"/bin/ycsb load grpcrocksdb \
+        -jvm-args="-Djava.library.path=${KVSTORE_HOME}:${MPI_LIB}" \
+        -P "$YCSB_HOME/workloads/$workload" \
+        -P "$PROFILE_PATH" \
+        -s -p grpc.addr="$SERVER:12345"
       kill_server
     else
       echo "Failed to create $db_file"
       exit 1
     fi
+  fi
 }
 
 for workload in ${WORKLOADS}; do
-  db_file="${DB_PREFIX}/${workload}"
-  curr_log_path="$LOG_PATH/${workload}.log"
-  curr_cpu_log_path="$LOG_PATH/${workload}.cpu.log"
-  kill_server
-  if [[ $CMD == "run" ]]; then
+  db_file="$(get_db_file "$workload")"
+
+  if [[ $CMD == "load" ]]; then
+    load "$workload"
+  elif [[ $CMD == "clean" ]]; then
+    echo "Remove $db_file on $SERVER"
+    ssh "$SERVER" rm -rf "$db_file"
+  elif [[ $CMD == "run" ]]; then
+    LOG_DIR=$(get_logs_dir)
+    curr_log_path="$LOG_DIR/${workload}.log"
+
     if [[ -f "$curr_log_path" ]]; then
-        echo "$curr_log_path exists, skip"
-      else
-        while true; do
-          # remove database
-          echo "Remove $db_file on $SERVER"
-          ssh "$SERVER" rm -rf "$db_file"
-
+      echo "$curr_log_path exists, skip"
+    else
+      while true; do
+        if ! ssh "$SERVER" "[ -d $db_file ]"; then
           load "$workload"
-          start_server "$workload"
-          ssh "$SERVER" "${SCRIPT_DIR}/cpu_usage_top.sh" kv_store "${curr_cpu_log_path}.tmp" &
+        fi
+        start_server "$workload"
 
+        tmp_host="/tmp/clients.$RANDOM"
+        if [[ -f $tmp_host ]]; then
           tmp_host="/tmp/clients.$RANDOM"
-          if [[ -f $tmp_host ]]; then
-            tmp_host="/tmp/clients.$RANDOM"
-          fi
-          tail -n +2 "$HOSTS_PATH" > "$tmp_host"
-          echo "============================= Running $workload with $NP clients"
-          # Evaluate
-          mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x RDMA_VERBOSITY \
-              -mca btl_tcp_if_include ib0 \
-              -wdir "$YCSB_HOME" -np $NP -hostfile "$tmp_host" \
-              python2 "$YCSB_HOME"/bin/ycsb run grpcrocksdb \
-              -jvm-args="-Djava.library.path=${KVSTORE_HOME}:${MPI_LIB}" \
-              -P "$YCSB_HOME/workloads/$workload" \
-              -P "$PROFILE_PATH" \
-              -s -p grpc.addr="$SERVER:12345" | tee "${curr_log_path}.tmp" 2>&1
-          ssh "$SERVER" pkill -U "$USER" top || true
-          kill_server
+        fi
+        tail -n +2 "$HOSTS_PATH" >"$tmp_host"
+        echo "============================= Running $workload with $NP clients, mode: $GRPC_PLATFORM_TYPE"
+        # Evaluate
+        mpirun --bind-to none -x GRPC_PLATFORM_TYPE -x RDMA_VERBOSITY \
+          -mca btl_tcp_if_include ib0 \
+          -wdir "$YCSB_HOME" -np "$NP" -hostfile "$tmp_host" \
+          python2 "$YCSB_HOME"/bin/ycsb run grpcrocksdb \
+          -jvm-args="-Djava.library.path=${KVSTORE_HOME}:${MPI_LIB}" \
+          -P "$YCSB_HOME/workloads/$workload" \
+          -P "$PROFILE_PATH" \
+          -s -p grpc.addr="$SERVER:12345" | tee "${curr_log_path}.tmp" 2>&1
+        kill_server
 
-          row_count=$(grep -c "\[OVERALL\], Throughput" < "${curr_log_path}.tmp")
+        row_count=$(grep -c "\[OVERALL\], Throughput" <"${curr_log_path}.tmp")
 
-          if [[ $row_count != "$NP" ]]; then
-            echo "Found error when evaluate workload $workload, retrying"
-          else
-            mv "${curr_log_path}.tmp" "${curr_log_path}"
-            mv "${curr_cpu_log_path}.tmp" "${curr_cpu_log_path}"
-            break
-          fi
-        done
-      fi
+        if [[ $row_count != "$NP" ]]; then
+          echo "Found error when evaluate workload $workload, retrying"
+        else
+          mv "${curr_log_path}.tmp" "${curr_log_path}"
+          break
+        fi
+      done
+    fi
   else
     echo "Bad command: $CMD"
   fi
